3a2b18af200fe9fa1e02f4581ef0368ba4834a69
Victor Cordova <azeta351@gmail.com>
2017-12-22 23:50:50 -0500

    Fixed MovieGrid's Cards width and height

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -28,34 +28,37 @@ class MovieCard extends React.Component {
     return (
       <div 
         className={styles.MovieCard}
-        style={{"backgroundImage": `url(${this.props.movie.backdropUrl || notFoundPlaceholder})`}}
         onClick={this.handleOnClick}
         onMouseEnter={() => this.handleOnMouseEnter()}
         onMouseLeave={() => this.setState({ showingDetails: false})}>
-        {this.state.showingDetails ? <div className={styles.overlay}/> : null}
-        {this.state.showingDetails ?
-          <div className={styles.content}>
-            <div className={styles.detailsContainer}>
-              <div className={styles.title}>{this.props.movie.title}</div>
-              <div className={styles.generalInfoContainer}>
-                <span className={styles.voteAverage}>{this.props.movie.voteAverage}</span>
-                <span className={styles.year}>{this.props.movie.year}</span>
-                {this.props.movie.adultFilm
-                  ? <span className={styles.adultFilm}>18+</span>
-                  : null
-                }
-                <span className={styles.duration}>{this.props.movie.duration}</span>
+        <div 
+          className={styles.movieCardInner}
+          style={{"backgroundImage": `url(${this.props.movie.backdropUrl || notFoundPlaceholder})`}}>
+            {this.state.showingDetails ? <div className={styles.overlay}/> : null}
+            {this.state.showingDetails ?
+              <div className={styles.content}>
+                <div className={styles.detailsContainer}>
+                  <div className={styles.title}>{this.props.movie.title}</div>
+                  <div className={styles.generalInfoContainer}>
+                    <span className={styles.voteAverage}>{this.props.movie.voteAverage}</span>
+                    <span className={styles.year}>{this.props.movie.year}</span>
+                    {this.props.movie.adultFilm
+                      ? <span className={styles.adultFilm}>18+</span>
+                      : null
+                    }
+                    <span className={styles.duration}>{this.props.movie.duration}</span>
+                  </div>
+                  <div className={styles.overview}>{this.props.movie.overview}</div>
+                </div>
+                <div className={styles.actionsContainer}>
+                  <div className={styles.action}><img src={thumbsUp} alt="ThumbsUp"/></div>
+                  <div className={styles.action}><img src={thumbsDown} alt="ThumbsDown"/></div>
+                  <div className={styles.action}><img src={plusSign} alt="PlusSign"/></div>
+                </div>
               </div>
-              <div className={styles.overview}>{this.props.movie.overview}</div>
-            </div>
-            <div className={styles.actionsContainer}>
-              <div className={styles.action}><img src={thumbsUp} alt="ThumbsUp"/></div>
-              <div className={styles.action}><img src={thumbsDown} alt="ThumbsDown"/></div>
-              <div className={styles.action}><img src={plusSign} alt="PlusSign"/></div>
-            </div>
-          </div>
-          : null
-        }
+              : null
+            }
+        </div>
       </div>
     );
   }

b1b626069df5f6c87121ec874ef7d4e27f26cc66
Victor Cordova <azeta351@gmail.com>
2018-04-13 13:04:36 -0500

    Fixed Logo link

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,7 +3,7 @@ import logo from './logo.png';
 import styles from './Logo.scss';
 
 const Logo = () => (
-  <a href="/" className={styles.Logo}>
+  <a href="#" className={styles.Logo}>
     <img src={logo} alt="Logo" className={styles.image}/>
   </a>
 );
